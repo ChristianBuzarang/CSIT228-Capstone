803b397c9afe6c82f9ffbbfc1aaa5c310d4bac7f
Admin <erickafatimareignrligaray@gmail.com>
2026-04-11 14:20:18 +0800

    trial

--- src/main/java/com/oop/gymquest/MemberRegisterApplication.java
+++ src/main/java/com/oop/gymquest/MemberRegisterApplication.java
@@ -5,14 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ProfileView extends Application {
+public class MemberRegisterApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("profile-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("member-register-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 500);
 
-        stage.setTitle("Profile View");
+        stage.setTitle("Member Registration");
         stage.setScene(scene);
         stage.show();
     }
